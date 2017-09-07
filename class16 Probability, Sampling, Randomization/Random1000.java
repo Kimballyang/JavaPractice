ec296da681140a8bf18358500f7a19ae1326a0a0
Kimballyang <ziruyang@gmail.com>
2017-09-07 16:01:53 -0400

    Update Random1000.java
    change “+” to "*"

--- class16 Probability, Sampling, Randomization/Random1000.java
+++ class16 Probability, Sampling, Randomization/Random1000.java
@@ -7,7 +7,7 @@ public class Random1000 {
 			//a0*x^0 + a1*x^2 + a3*x^3 +....+ak*x^k
 			int num = 0;
 			for(int i = 0; i <5; i++){
-				num = num + 5 + RandomFive.random5();
+				num = num * 5 + RandomFive.random5();
 			}
 			//choose 3000 instead of 1000 to reduce the # of expected random5() calls.
 			if (num < 3000){
