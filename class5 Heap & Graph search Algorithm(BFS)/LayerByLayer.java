e51b4629a352db648da1e3783ec5cca541b7b59f
Kimballyang <ziruyang@gmail.com>
2017-11-02 23:48:46 -0400

    Update LayerByLayer.java
    add

--- class5 Heap & Graph search Algorithm(BFS)/LayerByLayer.java
+++ class5 Heap & Graph search Algorithm(BFS)/LayerByLayer.java
@@ -1,5 +1,27 @@
-package laiofferClass5;
-
 public class LayerByLayer {
+	public List<List<Integer>> layerByLayer(TreeNode root){
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		if(root == null){
+			return list;
+		}
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			List<Integer> curLayer = new ArrayList<Integer>();
+			int size = queue.size();
+			for(int i = 0; i < size; i++){
+				TreeNode cur = queue.poll();
+				curLayer.add(cur.key);
+				if(cur.left != null){
+					queue.offer(cur.left);
+				}
+				if(cur.right != null){
+					queue.offer(cur.right);
+				}
+			}
+			list.add(curLayer);
+		}
+		return list;
+	}
 
 }
